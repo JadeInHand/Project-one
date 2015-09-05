be06b8a78b122af81580101b057ac4e5c9dabb02
JadeInHand <cpurcell22@gmail.com>
2015-09-05 14:32:55 +1000

    complete crud

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,7 @@
 Rails.application.routes.draw do
-  get 'parties/new'
-
-  get 'parties/index'
-
-  get 'parties/show'
-
-  get 'parties/edit'
-
   root :to => 'pages#home'
   resources :branches
   resources :pages
   resources :politicians
+  resources :parties
 end
